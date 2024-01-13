793c99070db403fb2a494185a9d2382784776984
Abdul Barik <abdulbarik1997m@gmail.com>
2024-01-14 00:26:41 +0600

    app/api/

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+import TopicsList from './components/TopicsList'
+// import Navbar from './components/Navbar'
+
+export default function Home() {
+ return <TopicsList />;
+}
