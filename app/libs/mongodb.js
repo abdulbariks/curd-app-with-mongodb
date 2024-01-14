6166a53168a8a658444e524a677804929b5df0e4
Abdul Barik <abdulbarik1997m@gmail.com>
2024-01-15 02:00:27 +0600

    curd app nextjs with mongodb

--- app/libs/mongodb.js
+++ app/libs/mongodb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    // await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect("mongodb://127.0.0.1:27017/curd-nextjs");
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
